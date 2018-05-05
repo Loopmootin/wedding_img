a17f456fcc11dca129e7d82a03599ae1c60fbbdf
Loopmootin <christoffera@outlook.dk>
2018-05-06 01:43:13 +0200

    first push

---
+++ ajax_php_file.php
@@ -0,0 +1,49 @@
+<?php
+    if(isset($_FILES["file"]["type"])) {
+
+        $validextensions = array("jpeg", "jpg", "png");
+        $temporary = explode(".", $_FILES["file"]["name"]);
+        $file_extension = end($temporary);
+        $target_dir = "img/";
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+        
+        if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
+        ) && ($_FILES["file"]["size"] < 10000000)//Approx. 10mb files can be uploaded.
+        && in_array($file_extension, $validextensions)) {
+
+            if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+
+        } else {
+
+            if (file_exists("img/" . $_FILES["file"]["name"])) {
+            echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+
+        } else {
+
+            $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+            $targetPath = "img/".$_FILES['file']['name']; // Target path where file is to be stored
+            move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+            echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+
+            require_once('db_con.php');
+            
+            $sql = 'INSERT INTO image(id, url) VALUES (?,?)';
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param('is', $id, $target_file);
+            $stmt->execute();
+            if ($stmt->affected_rows > 0){
+                echo 'Filedata added to the database :-)';
+            } else {
+                echo 'Could not add the file to the database';
+            }
+
+        }}} else {
+
+        echo "<span id='invalid'>***Invalid file Size or Type***<span>";
+
+        }
+    }
+
+?>
+
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>G&J Bryllup</title>
+
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		
+		<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+		<script src="main.js"></script>
+
+	</head>
+
+	<body>
+
+	<form id="uploadimage" action="" method="post" enctype="multipart/form-data">
+		<div id="selectImage">
+			<label>Her kan du uploade et nyt billede!</label><br />
+			<input type="file" name="file" id="file" required />
+			<input type="submit" value="Upload" class="submit" />
+		</div>
+	</form>
+
+	<div id="message"></div>
+
+	<div class="jumbotron">
+		<h1>G&J Bryllupsbilleder</h1>
+	</div>
+
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+			<img class="d-block h-100" src="img/CS3qhLhb.jpg" alt="First slide">
+			</div>
+			<div class="carousel-item">
+			<img class="d-block h-100" src="img/CS3qhLhb.jpg" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+			<img class="d-block h-100" src="img/CS3qhLhb.jpg" alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+
+	<?php
+
+	require_once('db_con.php');
+
+    $sql = 'SELECT * FROM image';
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    $stmt->bind_result($id, $name, $url);
+
+    while($stmt->fetch()){?>
+            <div class="uploadedImg">
+                <img src="<?=$url?>" width="auto" height="auto" />
+            </div>
+    <?php }?>
+</html>
